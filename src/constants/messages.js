b93995d04a8b843dd630f033cef63ae02842af35
noodle <noodle>
2023-11-25 16:01:38 +0900

    refactor : constants/message.js 분리

---
+++ src/constants/messages.js
@@ -0,0 +1,14 @@
+const input = Object.freeze({
+  numbers: '숫자를 입력해주세요 : ',
+});
+
+const output = Object.freeze({
+  init: '숫자 야구 게임을 시작합니다.\n',
+});
+
+const MESSAGES = Object.freeze({
+  input,
+  output,
+});
+
+export default MESSAGES;
