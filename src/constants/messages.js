0fa7de563810e741a402323d3a6b58582e62403b
noodle <noodle>
2023-11-26 00:53:04 +0900

    refactor : constants common과 messages 분리

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,9 +1,12 @@
 const input = Object.freeze({
   numbers: '숫자를 입력해주세요 : ',
+  retryOrQuit: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
 });
 
 const output = Object.freeze({
   init: '숫자 야구 게임을 시작합니다.\n',
+  quit: '게임 종료',
+  hitStrikes: '3개의 숫자를 모두 맞히셨습니다!',
 });
 
 const MESSAGES = Object.freeze({
