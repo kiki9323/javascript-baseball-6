d3138c470eb473f9b39e0c5d650d22114a6264ec
noodle <noodle>
2023-11-26 00:55:14 +0900

    refactor: controller/GameController.js 코드 분리

--- src/controller/GameController.js
+++ src/controller/GameController.js
@@ -1,9 +1,12 @@
 import AnswerGenerator from '../domain/AnswerGenerator.js';
-import { Console } from '@woowacourse/mission-utils';
+import AppError from '../errors/error.js';
+import COMMON from '../constants/common.js';
+import ERROR from '../constants/error.js';
+import GameCompare from '../domain/GameCompare.js';
+import GameResult from '../domain/GameResult.js';
 
 class GameController {
   #view;
-  #answerGenerator;
   #answer;
 
   constructor({ view }) {
@@ -11,80 +14,37 @@ class GameController {
   }
 
   async runGame() {
-    this.initMessage();
+    this.#view.printInitMessage();
     await this.gameStart();
   }
 
-  initMessage() {
-    this.#view.printInitMessage();
-  }
-
   async gameStart() {
-    this.#answerGenerator = new AnswerGenerator();
-    this.#answer = this.#answerGenerator.getAnswer();
+    this.#answer = AnswerGenerator.getAnswer();
     await this.playGameCycle();
   }
 
   async playGameCycle() {
     const validatedNumbers = await this.#view.readNumbers();
-
-    const { result, isFlag } = this.compareWithUserAnswer(this.#answer, validatedNumbers);
-
-    this.showResult(result);
-
-    isFlag ? await this.hitSrikes() : await this.playGameCycle();
+    const { result, isFlag } = GameCompare.compareWithUserAnswer(this.#answer, validatedNumbers);
+    GameResult.processingResult(result);
+    isFlag ? await this.chooseRetryOrQuit() : await this.playGameCycle();
   }
 
-  compareWithUserAnswer(answer, userAnswer) {
-    const result = { strikes: 0, balls: 0 };
-    let isFlag = false;
-
-    [...answer].forEach((digit, index) => {
-      if (digit === userAnswer[index]) result.strikes += 1;
-      else if (userAnswer.includes(digit)) result.balls += 1;
-    });
-
-    isFlag = result.strikes === 3 && result.balls === 0;
-
-    return { result, isFlag };
-  }
-
-  showResult(result) {
-    const { strikes, balls } = result;
-    let resultMessage = '';
-
-    if (balls > 0) resultMessage = `${balls}볼`;
-    if (strikes > 0) resultMessage += (resultMessage ? ' ' : '') + `${strikes}스트라이크`;
-    resultMessage = resultMessage || '낫싱';
-
-    Console.print(resultMessage);
-  }
-
-  async hitSrikes() {
-    this.gameEnd();
-    await this.chooseContinueOrQuit();
-  }
-
-  gameEnd() {
-    Console.print('3개의 숫자를 모두 맞히셨습니다!');
-  }
-
-  async chooseContinueOrQuit() {
-    const isReplay = await Console.readLineAsync(
-      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
-    );
+  async chooseRetryOrQuit() {
+    this.#view.printHitStrikes();
+    const isReplay = await this.#view.readRetryOrQuit();
 
     switch (isReplay) {
-      case '1': {
+      case COMMON.choose.retry: {
         await this.gameStart();
         break;
       }
-      case '2': {
-        Console.print('게임 종료');
+      case COMMON.choose.quit: {
+        this.#view.printEnd();
         break;
       }
       default: {
-        throw new Error('[ERROR] 올바른 선택을 입력하세요. 1 또는 2를 입력해주세요.');
+        throw new AppError(ERROR.retryExeption);
       }
     }
   }
